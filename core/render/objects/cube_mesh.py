acc25f41cc8f12c7446f7e90d677fd0bd2b6873f
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-03-24 23:47:46 +0100

    extracted initing mesh GL to mesh base class

--- core/render/objects/cube_mesh.py
+++ core/render/objects/cube_mesh.py
@@ -1,7 +1,5 @@
 from core.render.objects.mesh_base import MeshBase
 import numpy as np
-import OpenGL.GL as GL
-import ctypes
 
 
 class CubeMesh(MeshBase):
@@ -59,23 +57,3 @@ class CubeMesh(MeshBase):
         ), dtype=np.float32)
 
         super().__init__(self.vertices, position, eulers)
-
-    def init_gl(self):
-        self.vao = GL.glGenVertexArrays(1)
-        GL.glBindVertexArray(self.vao)
-
-        self.vbo = GL.glGenBuffers(1)
-
-        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.vbo)
-        GL.glBufferData(GL.GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL.GL_STATIC_DRAW)
-
-        GL.glEnableVertexAttribArray(0)
-        # vertexPos location in vertex shader | vertices count | FLOAT | FALSE | number fo bytes (6 x 4 bytes) |
-        # buffer stride in bytes
-        GL.glVertexAttribPointer(0, 3, GL.GL_FLOAT, GL.GL_FALSE, 24, ctypes.c_void_p(0))
-
-        # vertexColor location in vertex shader | colors count | FLOAT | FALSE | number fo bytes (6 x 4 bytes) |
-        # buffer stride in bytes
-
-        GL.glEnableVertexAttribArray(1)
-        GL.glVertexAttribPointer(1, 3, GL.GL_FLOAT, GL.GL_FALSE, 24, ctypes.c_void_p(12))
--- core/render/objects/mesh_base.py
+++ core/render/objects/mesh_base.py
@@ -1,5 +1,6 @@
 import pyrr
 import OpenGL.GL as GL
+import ctypes
 import numpy as np
 
 
@@ -20,8 +21,25 @@ class MeshBase:
         self.init_gl()
 
     def init_gl(self):
-        # Implemented by every mesh class
-        pass
+        self.vao = GL.glGenVertexArrays(1)
+        GL.glBindVertexArray(self.vao)
+
+        self.vbo = GL.glGenBuffers(1)
+
+        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.vbo)
+        GL.glBufferData(GL.GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL.GL_STATIC_DRAW)
+
+        GL.glEnableVertexAttribArray(0)
+
+        # vertexPos location in vertex shader | vertices count | FLOAT | FALSE | number fo bytes (6 x 4 bytes) |
+        # buffer stride in bytes
+        GL.glVertexAttribPointer(0, 3, GL.GL_FLOAT, GL.GL_FALSE, 24, ctypes.c_void_p(0))
+
+        # vertexColor location in vertex shader | colors count | FLOAT | FALSE | number fo bytes (6 x 4 bytes) |
+        # buffer stride in bytes
+        GL.glEnableVertexAttribArray(1)
+
+        GL.glVertexAttribPointer(1, 3, GL.GL_FLOAT, GL.GL_FALSE, 24, ctypes.c_void_p(12))
 
     def update(self):
         self.update_model_transformation()
