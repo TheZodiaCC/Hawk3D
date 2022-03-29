e8c05834472930c87b9cce1ae1cdba993b1b9c7e
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-03-29 23:37:21 +0200

    shaders files naming refactoring

--- core/render/consts.py
+++ core/render/consts.py
@@ -1,8 +1,11 @@
+import os
+
+
 class WindowConsts:
     WINDOW_WIDTH = 1280
     WINDOW_HEIGHT = 720
 
 
 class RenderConsts:
-    FRAGMENT_SHADER_DATA_PATH = "core/render/shaders/fragment.frag"
-    VERTEX_SHADER_DATA_PATH = "core/render/shaders/vertex.vert"
+    FRAGMENT_SHADER_DATA_PATH = os.path.join("core", "render", "shaders", "fragment.glsl")
+    VERTEX_SHADER_DATA_PATH = os.path.join("core", "render", "shaders", "vertex.glsl")
